f7f658da498c1b42a8d1991b20e299a33098a696
percevalseb1309 <percevalseb@gmail.com>
2017-11-10 19:51:09 +0100

    create a template

--- indexView.php
+++ indexView.php
@@ -1,34 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Blog Professionnel</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-<body>
-    <h1>Mon blog !</h1>
-    <p>Derniers billets du blog :</p>
+<?php $title = 'Blog professionnel'; ?>
 
-    <?php
-    while ($data = $posts->fetch())
-    {
-    ?>
-        <div class="news">
-            <h3>
-            	<?= htmlspecialchars($data['title']) ?><br>
-            	Par <?= htmlspecialchars($data['author']) ?> - 
-            	<em>dernière modification <?= $data['last_update'] ?></em>
-            </h3>
-                
-            <p>
-            	<strong><?= nl2br(htmlspecialchars($data['lead_paragraph'])) ?></strong>
-                <br>
-                <em><a href="post.php?id=<?= $data['id'] ?>">Voir le post en entier</a></em>
-        	</p>
-        </div>
-    <?php
-    }
-    $posts->closeCursor();
-    ?>
-</body>
-</html>
+<?php ob_start(); ?>
+<h1>Mon blog !</h1>
+<p>Derniers billets du blog :</p>
+
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+        	<?= htmlspecialchars($data['title']) ?><br>
+        	Par <?= htmlspecialchars($data['author']) ?> - 
+        	<em>dernière modification <?= $data['last_update'] ?></em>
+        </h3>
+            
+        <p>
+        	<strong><?= nl2br(htmlspecialchars($data['lead_paragraph'])) ?></strong>
+            <br>
+            <em><a href="post.php?id=<?= $data['id'] ?>">Voir le billet</a></em>
+    	</p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
--- postView.php
+++ postView.php
@@ -1,25 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Blog Professionnel</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-<body>
-    <h1>Mon blog !</h1>
-    <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php $title = htmlspecialchars($post['title']); ?>
 
-    <div class="news">
-        <h3>
-        	<?= htmlspecialchars($post['title']) ?><br>
-        	Par <?= htmlspecialchars($post['author']) ?> - 
-        	<em>dernière modification <?= $post['last_update'] ?></em>
-        </h3>
-            
-        <p>
-        	<strong><?= nl2br(htmlspecialchars($post['lead_paragraph'])) ?></strong>
-        	<?= nl2br(htmlspecialchars($post['content'])) ?>
-    	</p>
-    </div>
-</body>
-</html>
+<?php ob_start(); ?>
+<h1>Mon blog !</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
+
+<div class="news">
+    <h3>
+    	<?= htmlspecialchars($post['title']) ?><br>
+    	Par <?= htmlspecialchars($post['author']) ?> - 
+    	<em>dernière modification <?= $post['last_update'] ?></em>
+    </h3>
+        
+    <p>
+    	<strong><?= nl2br(htmlspecialchars($post['lead_paragraph'])) ?></strong>
+    	<?= nl2br(htmlspecialchars($post['content'])) ?>
+	</p>
+</div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
