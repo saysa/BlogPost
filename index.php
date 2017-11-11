7866e64c0fcd3816e3d064ae1946929ac6a0593e
percevalseb1309 <percevalseb@gmail.com>
2017-11-11 16:17:46 +0100

    add a new feature, update a post

--- index.php
+++ index.php
@@ -12,6 +12,27 @@ if (isset($_GET['action'])) {
         else {
             echo 'Erreur : aucun identifiant de billet envoyé';
         }
+    }    
+    elseif ($_GET['action'] == 'postForm') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            postForm();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+    elseif ($_GET['action'] == 'editPost') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            if ( ! empty($_POST['author']) && ! empty($_POST['title']) && ! empty($_POST['lead_paragraph']) && ! empty($_POST['content'])) {
+                editPost($_POST['author'], $_POST['title'], $_POST['lead_paragraph'], $_POST['content'], $_GET['id']);
+            }
+            else {
+                echo 'Erreur : tous les champs ne sont pas remplis !';
+            }
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
     }
 }
 else {
