62d494877a7b68e0fdf5093855dd1d195001d30f
percevalseb1309 <percevalseb@gmail.com>
2017-11-12 20:42:03 +0100

    model improvement, sql error display

--- model/model.php
+++ model/model.php
@@ -12,16 +12,23 @@ function getPost($postId)
     $db = dbConnect();
     $req = $db->prepare('SELECT id, author, title, lead_paragraph, content, DATE_FORMAT(last_update, \'%d/%m/%Y à %Hh%imin%ss\') AS last_update FROM post WHERE id = ?');
     $req->execute(array($postId));
-    $post = $req->fetch();
-
-    return $post;
+    
+    if ($req->rowCount() == 1) 
+        return $req->fetch();
+    else
+        throw new Exception("Aucun billet ne correspond à l'identifiant '$postId'");
 }
 
 function addPost($author, $title, $lead_paragraph, $content)
 {
     $db = dbConnect();
-    $post = $db->prepare('INSERT INTO post (author, title, lead_paragraph, content) VALUES (?, ?, ?, ?)');
-    $affectedLines = $post->execute(array($author, $title, $lead_paragraph, $content));
+    $post = $db->prepare('INSERT INTO post (author, title, lead_paragraph, content) VALUES (:author, :title, :lead_paragraph, :content)');
+    $affectedLines = $post->execute(array(
+        'author'         => $author, 
+        'title'          => $title, 
+        'lead_paragraph' => $lead_paragraph, 
+        'content'        => $content
+    ));
 
     return $affectedLines;
 }
@@ -29,14 +36,20 @@ function addPost($author, $title, $lead_paragraph, $content)
 function updatePost($postId, $author, $title, $lead_paragraph, $content)
 {
     $db = dbConnect();
-    $post = $db->prepare('UPDATE post SET author = ?, title = ?, lead_paragraph = ?, content = ?, last_update = NOW() WHERE id = ?');
-    $affectedLines = $post->execute(array($author, $title, $lead_paragraph, $content, $postId));
+    $post = $db->prepare('UPDATE post SET author = :author, title = :title, lead_paragraph = :lead_paragraph, content = :content, last_update = NOW() WHERE id = :id');
+    $affectedLines = $post->execute(array(
+        'id'             => $postId,
+        'author'         => $author, 
+        'title'          => $title, 
+        'lead_paragraph' => $lead_paragraph, 
+        'content'        => $content
+    ));
 
     return $affectedLines;
 }
 
 function dbConnect()
 {
-    $db = new PDO('mysql:host=localhost;dbname=blog_post;charset=utf8', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=blog_post;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $db;
 }
