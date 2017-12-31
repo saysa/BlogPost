260144ad3debc82b157aac7d250d5b6ab708c802
percevalseb1309 <percevalseb@gmail.com>
2017-12-31 15:06:01 +0100

    sending a fictitious mail

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -4,8 +4,18 @@ namespace OC\BlogPost\Controller;
 require_once('framework/Controller.php');
 use \OC\BlogPost\Framework\Controller;
 
+/*require_once('model/PostManager.php');
+use \OC\BlogPost\Model\PostManager;*/
+
+require_once('framework/Configuration.php');
+use \OC\BlogPost\Framework\Configuration;
+
 class HomeController extends Controller
 {
+    private static $_mailerTransport;
+    const PROTOCOL = 'ssl';
+    const PORT     = 465;
+
     public function __construct(\Twig_Environment $twig) 
     {
         parent::__construct($twig);
@@ -16,11 +26,36 @@ class HomeController extends Controller
         $this->generateView('home');
     }    
 
+    private static function mailerTransport()
+    {
+        if (SELF::$_mailerTransport === null) {
+            $host     = Configuration::get("mailer_host"); 
+            $user     = Configuration::get("mailer_user");
+            $password = Configuration::get("mailer_password");
+            SELF::$_mailerTransport = (new \Swift_SmtpTransport($host, SELF::PORT, SELF::PROTOCOL))
+                ->setUsername($user)
+                ->setPassword($password);
+        }
+        return SELF::$_mailerTransport;
+    }
+
     public function contact()
     {
-		$name       = $this->_request->getParameter("name");
-		$first_name = $this->_request->getParameter("first_name");
-		$email      = $this->_request->getParameter("email");
-		$message    = $this->_request->getParameter("message");
+        $name       = $this->_request->getParameter("name");
+        $first_name = $this->_request->getParameter("first_name");
+        $email      = $this->_request->getParameter("email");
+        $message    = $this->_request->getParameter("message");
+        $username   = $first_name.' '.$name;
+
+        $message = (new \Swift_Message('Message de '.$username))
+          ->setFrom([$email => $username])
+          ->setTo('percevalseb@gmail.com')
+          ->setBody($message, 'text/html');
+
+        $transport = SELF::mailerTransport();
+        
+        $mailer = new \Swift_Mailer($transport);
+
+        $mailer->send($message);
     }
 }
--- framework/Manager.php
+++ framework/Manager.php
@@ -7,6 +7,7 @@ use \OC\BlogPost\Framework\Configuration;
 abstract class Manager
 {
 	private static $_db;
+	private static $_mailer;
 
 	protected function executeRequest($sql, $params = null) {
 		if ($params == null) {
@@ -23,10 +24,23 @@ abstract class Manager
     {
     	if (SELF::$_db === null) {
 			$dsn      = Configuration::get("dsn"); 
-			$login    = Configuration::get("login");
-			$password = Configuration::get("password");
+			$login    = Configuration::get("db_login");
+			$password = Configuration::get("db_password");
 	    	SELF::$_db = new \PDO($dsn, $login, $password, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
 	    }
     	return SELF::$_db;
+    }    
+
+    private static function mailerConnect()
+    {
+    	if (SELF::$_mailer === null) {
+			$host     = Configuration::get("mailer_host"); 
+			$user     = Configuration::get("mailer_user");
+			$password = Configuration::get("mailer_password");
+	    	SELF::$_mailer = (new \Swift_SmtpTransport($host, 465, 'ssl'))
+	            ->setUsername($user)
+	            ->setPassword($password);
+	    }
+    	return SELF::$_mailer;
     }
 }
